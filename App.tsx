433866367eda9b2a379b4d6fbe3970b07f5a687b
xh-lin <xuhuanglin99@gmail.com>
2024-01-25 20:09:04 -0500

    core components

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App(): React.JSX.Element {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>Hello</Text>
+      </View>
+      <Text>World!</Text>
+      <Button title="Tap me!" />
     </View>
   );
 }
