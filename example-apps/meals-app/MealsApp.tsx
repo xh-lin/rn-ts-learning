fef655eeece371f07ce6d67249df767effbbc96b
xh-lin <xuhuanglin99@gmail.com>
2024-01-28 11:29:40 -0500

    app background color

--- example-apps/meals-app/MealsApp.tsx
+++ example-apps/meals-app/MealsApp.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import CategoriesScreen from './screens/CategoriesScreen';
+import { StyleSheet, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
 export default function MealsApp(): JSX.Element {
-  return <CategoriesScreen />;
+  return (
+    <View style={styles.appContainer}>
+      <StatusBar style="light" />
+      <CategoriesScreen />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+    backgroundColor: '#24180f', // Replace with your desired background color
+  },
+});
--- example-apps/meals-app/components/CategoryGridTile.tsx
+++ example-apps/meals-app/components/CategoryGridTile.tsx
@@ -19,7 +19,7 @@ export default function CategoryGridTile({
           pressed ? styles.buttonPressed : null,
         ]}
       >
-        <View style={styles.innerContainer}>
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
           <Text style={styles.title}>{title}</Text>
         </View>
       </Pressable>
@@ -50,6 +50,7 @@ const styles = StyleSheet.create({
   innerContainer: {
     flex: 1,
     padding: 16,
+    borderRadius: 8,
     justifyContent: 'center',
     alignItems: 'center',
   },
