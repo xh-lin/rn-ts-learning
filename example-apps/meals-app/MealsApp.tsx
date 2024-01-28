d5d80726f463047a486dd3d8d3c4934e6f9c1757
xh-lin <xuhuanglin99@gmail.com>
2024-01-28 12:38:21 -0500

    set up React Navigation package

--- example-apps/meals-app/MealsApp.tsx
+++ example-apps/meals-app/MealsApp.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import CategoriesScreen from './screens/CategoriesScreen';
 import { StyleSheet, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function MealsApp(): JSX.Element {
   return (
     <View style={styles.appContainer}>
       <StatusBar style="light" />
-      <CategoriesScreen />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
