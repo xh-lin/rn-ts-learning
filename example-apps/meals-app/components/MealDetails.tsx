ef96a99567db47e61b36a5d602280d87a6df30b9
xh-lin <xuhuanglin99@gmail.com>
2024-01-28 20:16:02 -0500

    styling Meal Details

--- example-apps/meals-app/components/MealDetails.tsx
+++ example-apps/meals-app/components/MealDetails.tsx
@@ -5,18 +5,26 @@ interface IMealDetailsProps {
   duration: string;
   complexity: string;
   affordability: string;
+  style?: any;
+  textStyle?: any;
 }
 
 export default function MealDetails({
   duration,
   complexity,
   affordability,
+  style,
+  textStyle,
 }: IMealDetailsProps): JSX.Element {
   return (
-    <View style={styles.details}>
-      <Text style={styles.detailItem}>{duration}m</Text>
-      <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
-      <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+    <View style={[styles.details, style]}>
+      <Text style={[styles.detailItem, textStyle]}>{duration}m</Text>
+      <Text style={[styles.detailItem, textStyle]}>
+        {complexity.toUpperCase()}
+      </Text>
+      <Text style={[styles.detailItem, textStyle]}>
+        {affordability.toUpperCase()}
+      </Text>
     </View>
   );
 }
--- example-apps/meals-app/screens/MealDetailScreen.tsx
+++ example-apps/meals-app/screens/MealDetailScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 import MealDetails from '../components/MealDetails';
 import { MEALS } from '../data/dummy-data';
@@ -19,21 +19,51 @@ export default function MealDetailScreen({
 
   return (
     <View>
-      <Image source={{ uri: selectedMeal?.imageUrl }} />
-      <Text>{selectedMeal?.title}</Text>
+      <Image style={styles.image} source={{ uri: selectedMeal?.imageUrl }} />
+      <Text style={styles.title}>{selectedMeal?.title}</Text>
       <MealDetails
         duration={selectedMeal?.duration}
         complexity={selectedMeal?.complexity}
         affordability={selectedMeal?.affordability}
+        textStyle={styles.detailText}
       />
-      <Text>Ingredients</Text>
+      <Text style={styles.subtitle}>Ingredients</Text>
       {selectedMeal?.ingredients.map((ingredient: string) => (
         <Text key={ingredient}>{ingredient}</Text>
       ))}
-      <Text>Steps</Text>
+      <Text style={styles.subtitle}>Steps</Text>
       {selectedMeal?.steps.map((step: string) => (
         <Text key={step}>{step}</Text>
       ))}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: 350,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    margin: 8,
+    textAlign: 'center',
+    color: 'white',
+  },
+  detailText: {
+    color: 'white',
+  },
+  subtitle: {
+    color: '#e2b497',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    padding: 6,
+    margin: 4,
+    marginHorizontal: 24,
+    marginVertical: 4,
+    borderBottomColor: '#e2b497',
+    borderBottomWidth: 2,
+  },
+});
