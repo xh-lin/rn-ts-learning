507383a69214a3af8d51ef7abd58cc723f9fb34d
xh-lin <xuhuanglin99@gmail.com>
2024-01-30 18:37:55 -0500

    bottom tabs and finish app

--- example-apps/meals-app/navigation/Navigation.tsx
+++ example-apps/meals-app/navigation/Navigation.tsx
@@ -1,3 +1,4 @@
+import { Ionicons } from '@expo/vector-icons';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -20,13 +21,32 @@ function DrawerNavigator(): JSX.Element {
         headerStyle: { backgroundColor: '#351401' },
         headerTintColor: 'white',
         sceneContainerStyle: { backgroundColor: '#3f2f25' },
+        drawerContentStyle: {
+          backgroundColor: '#351401',
+        },
+        drawerInactiveTintColor: 'white',
+        drawerActiveTintColor: '#351401',
+        drawerActiveBackgroundColor: '#e4baa1',
       }}
     >
       <Drawer.Screen
         name={Route.MealsCategories}
         component={CategoriesScreen}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="list" color={color} size={size} />
+          ),
+        }}
       />
-      <Drawer.Screen name={'Favorites'} component={FavoritesScreen} />
+      <Drawer.Screen
+        name={'Favorites'}
+        component={FavoritesScreen}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="star" color={color} size={size} />
+          ),
+        }}
+      />
     </Drawer.Navigator>
   );
 }
