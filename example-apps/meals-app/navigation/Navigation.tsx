3f11fae917de25e89420c6ba2ff93b9616e3e6c1
xh-lin <xuhuanglin99@gmail.com>
2024-01-30 18:29:19 -0500

    nesting navigators

--- example-apps/meals-app/navigation/Navigation.tsx
+++ example-apps/meals-app/navigation/Navigation.tsx
@@ -1,14 +1,35 @@
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 
 import { Route } from '../enums/Route';
 import CategoriesScreen from '../screens/CategoriesScreen';
+import FavoritesScreen from '../screens/FavoritesScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
 import MealsOverviewScreen from '../screens/MealsOverviewScreen';
 import type NativeStackParamList from './NativeStackParamList';
 
 const Stack = createNativeStackNavigator<NativeStackParamList>();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator(): JSX.Element {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: '#351401' },
+        headerTintColor: 'white',
+        sceneContainerStyle: { backgroundColor: '#3f2f25' },
+      }}
+    >
+      <Drawer.Screen
+        name={Route.MealsCategories}
+        component={CategoriesScreen}
+      />
+      <Drawer.Screen name={'Favorites'} component={FavoritesScreen} />
+    </Drawer.Navigator>
+  );
+}
 
 export default function Navigation(): JSX.Element {
   return (
@@ -21,31 +42,17 @@ export default function Navigation(): JSX.Element {
         }}
       >
         <Stack.Screen
-          name={Route.MealsCategories}
-          component={CategoriesScreen}
-          // options={{
-          //   title: Route.MealsCategories,
-          // }}
+          name={'Drawer'}
+          component={DrawerNavigator}
+          options={{
+            headerShown: false,
+          }}
         />
         <Stack.Screen
           name={Route.MealsOverview}
           component={MealsOverviewScreen}
-          // options={({ route, navigation }) => {
-          //   const catId = route.params.categoryId;
-          //   return {
-          //     title: catId,
-          //   };
-          // }}
         />
-        <Stack.Screen
-          name={Route.MealDetail}
-          component={MealDetailScreen}
-          // options={{
-          //   headerRight: () => {
-          //     return <Text>In the header</Text>;
-          //   },
-          // }}
-        />
+        <Stack.Screen name={Route.MealDetail} component={MealDetailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ example-apps/meals-app/screens/FavoritesScreen.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export default function FavoritesScreen(): JSX.Element {
+  return <Text>FavoritesScreen</Text>;
+}
